d594a5319868770c29dc4a68ddd29145bef5859a
Lance Hudson <lancexhudson@gmail.com>
2023-04-24 21:29:48 -0400

    updating css. this. is. difficult.

--- src/components/SliderData.js
+++ src/components/SliderData.js
@@ -1,7 +1,7 @@
 export const SliderData = [
   {
     image:
-      'https://user-images.githubusercontent.com/106623072/233862208-c9062de8-675b-43a0-85f9-39f3adb44352.jpeg',
+      'https://user-images.githubusercontent.com/106623072/234151705-69976623-fe83-476e-a827-82d91db89fb9.jpeg',
   },
   {
     image:
