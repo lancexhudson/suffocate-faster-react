093d1c2c25fc291ffa4be0156f6d8887ac6f0188
Lance Hudson <lancexhudson@gmail.com>
2023-04-28 06:55:50 -0400

    figuring out bio css - continues to revert back to 310 width in about and members..

--- src/components/HeroBio.js
+++ src/components/HeroBio.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import '../App.css';
+// import '../App.css';
 import './HeroBio.css';
 import { Button } from './Button';
 
@@ -11,21 +11,22 @@ function HeroBio() {
       {/* </div> */}
       <div className="artwork-container">
         <p id="about">
-          SUFFOCATE FASTER IS A STRAIGHT EDGE HARDCORE BAND FROM CINCINNATI
-          OHIO, USA. FORMED IN 2001, THE BAND HAS TOURED THE U.S. WITH BANDS
-          LIKE BURY YOUR DEAD AND TERROR. THE BAND AS TWO FULL LENGTHS AND 2 EPS
-          RELEASED.
+          SUFFOCATE FASTER IS A STRAIGHT EDGE HARDCORE BAND FROM CINCINNATI,
+          OHIO, USA. SINCE 2001, THE BAND HAS RELEASED TWO FULL-LENGTH ALBUMS
+          AND IS CURRENTLY SIGNED WITH SMARTPUNK RECORDS WITH TWO EPS SLATED FOR
+          RELEASE IN 2023.
         </p>
         <img
           src="https://user-images.githubusercontent.com/106623072/234729894-009cb551-d4b8-47e3-8080-3cfa92aaa04c.jpg"
           alt="promo"
           id="promo-pic"
         />
+
+        <p id="members">
+          SUFFOCATE FASTER IS: CHRIS TOWNING, CHRIS ALSIP, LANCE HUDSON, AND
+          COREY WILLIAMS
+        </p>
       </div>
-      <p id="members">
-        SUFFOCATE FASTER IS: CHRIS TOWNING, CHRIS ALSIP, LANCE HUDSON, AND COREY
-        WILLIAMS
-      </p>
     </div>
   );
 }
