9e3e5eb9015402280850d5ca87b2a5fff02f3b90
Lance Hudson <lancexhudson@gmail.com>
2023-05-16 19:10:50 -0400

    shows updated

--- src/components/SliderData.js
+++ src/components/SliderData.js
@@ -7,4 +7,8 @@ export const SliderData = [
     image:
       'https://user-images.githubusercontent.com/106623072/233873241-64a0d6fa-a927-47cb-bbf0-78f7f20c93cb.jpeg',
   },
+  {
+    image:
+      'https://private-user-images.githubusercontent.com/106623072/238790447-66280170-6e71-41b5-917f-ea44d38c95f9.jpeg?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJrZXkiOiJrZXkxIiwiZXhwIjoxNjg0Mjc4NzU4LCJuYmYiOjE2ODQyNzg0NTgsInBhdGgiOiIvMTA2NjIzMDcyLzIzODc5MDQ0Ny02NjI4MDE3MC02ZTcxLTQxYjUtOTE3Zi1lYTQ0ZDM4Yzk1ZjkuanBlZz9YLUFtei1BbGdvcml0aG09QVdTNC1ITUFDLVNIQTI1NiZYLUFtei1DcmVkZW50aWFsPUFLSUFJV05KWUFYNENTVkVINTNBJTJGMjAyMzA1MTYlMkZ1cy1lYXN0LTElMkZzMyUyRmF3czRfcmVxdWVzdCZYLUFtei1EYXRlPTIwMjMwNTE2VDIzMDczOFomWC1BbXotRXhwaXJlcz0zMDAmWC1BbXotU2lnbmF0dXJlPTBhODIzMWJjZjVlY2QzZjg5YTdkNWNkM2Q3MjYwYWRiZDdmOTMzMjI3ZmJiNjg2NzQ1YmEyMjc2MGJlOWZlMmMmWC1BbXotU2lnbmVkSGVhZGVycz1ob3N0In0.h_oTvovXG0CuTn5l_6zbRcmNmZ7gqa-VtkK2BEuYwsc',
+  },
 ];
