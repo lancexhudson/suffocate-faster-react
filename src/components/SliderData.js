1a831e973d8998e45a36a3c0446d2474b6a074cb
Lance Hudson <lancexhudson@gmail.com>
2023-05-16 21:29:14 -0400

    fixed bad flyer. Check to see if repo needs to be public to share issue-photo

--- src/components/SliderData.js
+++ src/components/SliderData.js
@@ -9,6 +9,6 @@ export const SliderData = [
   },
   {
     image:
-      'https://private-user-images.githubusercontent.com/106623072/238790447-66280170-6e71-41b5-917f-ea44d38c95f9.jpeg?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJrZXkiOiJrZXkxIiwiZXhwIjoxNjg0Mjc4NzU4LCJuYmYiOjE2ODQyNzg0NTgsInBhdGgiOiIvMTA2NjIzMDcyLzIzODc5MDQ0Ny02NjI4MDE3MC02ZTcxLTQxYjUtOTE3Zi1lYTQ0ZDM4Yzk1ZjkuanBlZz9YLUFtei1BbGdvcml0aG09QVdTNC1ITUFDLVNIQTI1NiZYLUFtei1DcmVkZW50aWFsPUFLSUFJV05KWUFYNENTVkVINTNBJTJGMjAyMzA1MTYlMkZ1cy1lYXN0LTElMkZzMyUyRmF3czRfcmVxdWVzdCZYLUFtei1EYXRlPTIwMjMwNTE2VDIzMDczOFomWC1BbXotRXhwaXJlcz0zMDAmWC1BbXotU2lnbmF0dXJlPTBhODIzMWJjZjVlY2QzZjg5YTdkNWNkM2Q3MjYwYWRiZDdmOTMzMjI3ZmJiNjg2NzQ1YmEyMjc2MGJlOWZlMmMmWC1BbXotU2lnbmVkSGVhZGVycz1ob3N0In0.h_oTvovXG0CuTn5l_6zbRcmNmZ7gqa-VtkK2BEuYwsc',
+      'https://github.com/lancexhudson/tight-grip-react/assets/106623072/09e28b9e-f3e9-41a6-9964-ab8c977ab949',
   },
 ];
