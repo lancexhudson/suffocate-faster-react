e768c26e170ec272159b72c45441dabc111e71d3
Lance Hudson <lancexhudson@gmail.com>
2023-05-17 21:19:56 -0400

    this damn audio player..

---
+++ src/components/AudioPlayer.js
@@ -0,0 +1,27 @@
+import React, { useState, useRef } from 'react';
+import lostOnes from '../assets/lost-ones.mp3';
+
+function AudioPlayer({ lostOnes }) {
+  const [isPlaying, setIsPlaying] = useState(false);
+  const audioRef = useRef(null);
+
+  const handlePlayPause = () => {
+    if (!audioRef.current) return;
+
+    if (isPlaying) {
+      audioRef.current.pause();
+    } else {
+      audioRef.current.play();
+    }
+    setIsPlaying(!isPlaying);
+  };
+
+  return (
+    <div>
+      <audio ref={audioRef} src={lostOnes} />
+      <button onClick={handlePlayPause}>{isPlaying ? 'Pause' : 'Play'}</button>
+    </div>
+  );
+}
+
+export default AudioPlayer;
--- src/components/HeroHome.js
+++ src/components/HeroHome.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useRef, useState, useEffect } from 'react';
-
+// import AudioPlayer from './AudioPlayer';
 import '../App.css';
 import { Button } from './Button';
 import './HeroHome.css';
@@ -11,7 +11,7 @@ import PlayerLogic from '../player/PlayerLogic';
 // import Player from '../player/Player';
 import { Link } from 'react-router-dom';
 import HeroShows from './HeroShows';
-import Player from './Player';
+import Player from './AudioPlayer';
 
 function HeroHome() {
   // function play() {
@@ -34,17 +34,15 @@ function HeroHome() {
       <p id="upcoming">FROM THE UPCOMING EP</p>
       <p id="title">THIS IS THE WAY VOL. 1</p>
       <div className="album-container">
-        {/* <div className="artwork-container"> */}
         <img src={epVinyl} alt="" className="artwork" />
-        {/* <img src={artwork} alt="" class="artwork-alt" /> */}
-        {/* </div> */}
+
         <div className="hero-btns">
           <Button
             className="btns"
             buttonStyle="btn--outline"
             buttonSize="btn--large"
             id="toggle"
-            // onClick={playPause}
+            //onClick={togglePlay}
             onClick={(e) => {
               e.preventDefault();
               window.location.href =
